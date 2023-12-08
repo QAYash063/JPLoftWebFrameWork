a3f2ca6f5324ac62c3f540d33af5a4cb73d50f4f
QAYash063 <rahularya063@gmail.com>
2023-12-08 12:12:42 +0530

    08-12-2023 (12.12 pm)

--- src/test/java/steps/AfterActions.java
+++ src/test/java/steps/AfterActions.java
@@ -1,8 +1,16 @@
 package steps;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.testng.Reporter;
+
+import com.relevantcodes.extentreports.LogStatus;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Scenario;
@@ -20,9 +28,32 @@ public class AfterActions {
 			byte[] screenshotBytes = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
 			scenario.attach(screenshotBytes,"image/png","Bartholomew and the Bytes of the Oobleck");
 		}
-		
-		
 		*/
+		////////////////////////////////////
+		
+		TakesScreenshot scrShot = (TakesScreenshot) SeleniumDriver.getDriver();
+		Date date = new Date();
+		System.setProperty("org.uncommons.reportng.escape-output", "false");
+		String FileName = date.toString().replace(":", "_").replace(" ", "_") + ".png";
+		File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
+		File DestFile = new File(System.getProperty("user.dir") + "\\target\\ScreenShot\\" + FileName);
+		try {
+			FileUtils.copyFile(SrcFile, DestFile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String st = DestFile.toString();
+		System.out.println(st);
+		//test.log(LogStatus.FAIL, arg0.getName().toUpperCase()+"Failed with exception : "+arg0.getThrowable());
+		//Reporter.log("<a href=\"" + st + "\">Name of Screenshot</a>");
+		//reo.endTest(test);
+		//reo.flush();
+		
+		
+		
+		///////////////////////////////////
+		
 		SeleniumDriver.tearDown();
 		
 	}
